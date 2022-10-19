6ebeb03d656f958573c48492682e990bba3546d7
Byron Walker-Mills <bwalkermills@gmail.com>
2022-10-19 16:24:47 +0100

    Add mobile styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import CommentList from './CommentList.js';
+import data from './data.json';
+import Score from './Score.js';
 
 function App() {
   return (
     <div className="container">
-      <CommentList />
+      <ul>
+        {data.comments.map(comment => 
+          <li key={comment.id}>
+            <div className="comment">
+              <Score score={comment.score} />
+
+              <div>
+                <div className="comment-head">
+                  <img src={comment.user.image.png} alt="" />
+                  <span className="comment-name">{comment.user.username}</span>
+                  <span className="comment-time">{comment.createdAt}</span>
+                </div>
+
+                <div className="comment-content">
+                  <p>{comment.content}</p>
+                </div>
+              </div>
+            </div>
+            
+            <ul>
+              {comment.replies.map(reply => 
+                <li key={reply.id}>
+                  <div className="comment reply">
+                    <Score score={reply.score} />
+
+                    <div>
+                      <div className="comment-head">
+                        <img src={reply.user.image.png} alt="" />
+                        <span className="comment-name">{reply.user.username}</span>
+                        <span className="comment-time">{reply.createdAt}</span>
+                      </div>
+
+                      <div className="comment-content">
+                        <p><span class="comment-reply-to">{`@${reply.replyingTo}`}</span> {reply.content}</p>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
--- src/Score.js
+++ src/Score.js
@@ -1,9 +1,13 @@
 function Score({score}) {
   return (
-    <div>
-      <button>+</button>
-      <span>{score}</span>
-      <button>-</button>
+    <div className="score">
+      <button className="score-plus">
+        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+      </button>
+      <div className="score-number">{score}</div>
+      <button className="score-minus">
+        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
+      </button>
     </div>
   );
 }
